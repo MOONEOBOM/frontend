d70ed181b85fff81d9ccb26a95ff35e2c0a9c15e
Park yejin <92977621+yezzan9@users.noreply.github.com>
2026-01-15 11:30:17 +0900

    feat: 뱃지 컴포넌트 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import { useState } from 'react';
 import { User } from 'firebase/auth';
 import Button from '@/components/common/Button';
 import GoogleIcon from '@/assets/icon/google.svg';
+import Badge from '@/components/common/Badge';
+
 function App() {
   const [userData, setUserData] = useState<User | null>(null);
   function handleGoogleLogin() {
@@ -39,6 +41,9 @@ function App() {
       >
         구글로 로그인
       </Button>
+
+      <Badge type="blue">대학생 요금제 추천해줘</Badge>
+      <Badge type="primary">요금제 변경</Badge>
     </div>
   );
 }
---
+++ src/components/common/Badge.tsx
@@ -0,0 +1,20 @@
+const Badge = ({
+  children,
+  type,
+}: {
+  children: React.ReactNode;
+  type: 'blue' | 'primary';
+}) => {
+  const baseStyle = 'flex items-center rounded-full w-fit body2 leading-none';
+  const typeStyles = {
+    blue: 'px-[10px] py-[6px] bg-secondary',
+    primary:
+      'box-border px-[14px] py-[8px]  bg-primary-100 border-[1px] border-primary',
+  };
+
+  const className = `${baseStyle} ${typeStyles[type]}`;
+
+  return <div className={className}>{children}</div>;
+};
+
+export default Badge;
