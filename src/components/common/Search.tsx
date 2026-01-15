5fd441a1b037cb401d5f11e40e351fe1a4b7eabf
moon <gkdlrne97@naver.com>
2026-01-15 10:35:06 +0900

    fix: searchIcon 오타 수정

--- src/components/common/Search.tsx
+++ src/components/common/Search.tsx
@@ -1,12 +1,12 @@
 import { Input } from './Input';
-import SerrchIcon from '@/assets/icon/search.svg';
+import SearchIcon from '@/assets/icon/search.svg';
 
 export const Search = (props: React.ComponentProps<typeof Input>) => {
   const baseCls = ['pl-[40px]', 'body1'].join(' ');
   return (
     <div className="relative w-full ">
       <span className="pointer-events-none absolute left-[14px] top-1/2 -translate-y-1/2 text-gray-400">
-        <SerrchIcon />
+        <SearchIcon />
       </span>
 
       <Input
