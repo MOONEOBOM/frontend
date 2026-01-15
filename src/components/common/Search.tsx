acb1241ada4baf89a8dd6a085726bda7a17a0944
moon <gkdlrne97@naver.com>
2026-01-15 14:56:29 +0900

    fix: Search컴포넌트 props 수정

--- src/components/common/Search.tsx
+++ src/components/common/Search.tsx
@@ -5,19 +5,18 @@ import SearchIcon from '@/assets/icon/search.svg?react';
 export const Search = forwardRef<
   HTMLInputElement,
   React.ComponentProps<typeof Input>
->((props, ref) => {
+>(({ className, width = 'w-full', ...props }, ref) => {
   const baseCls = 'pl-[40px] body1';
 
   return (
-    <div className="relative w-full ">
+    <div className="relative ${width} ">
       <SearchIcon className="pointer-events-none absolute left-[14px] top-1/2 -translate-y-1/2 text-gray-400" />
 
       <Input
         ref={ref}
         variant="search"
-        className={['pl-[40px] body1', props.className]
-          .filter(Boolean)
-          .join(' ')}
+        width={width}
+        className={[baseCls, className].filter(Boolean).join(' ')}
         {...props}
       />
     </div>
