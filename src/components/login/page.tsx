d1e0e95b345b7ca5047ca058e2f5db6402279c3f
dahlia0916 <dahlia3511@hanyang.ac.kr>
2026-01-16 14:21:43 +0900

    Feat: 로그인 페이지 제작

--- src/components/login/page.tsx
+++ src/components/login/page.tsx
@@ -1,7 +1,44 @@
+'use client';
+import Logo from '@/assets/icon/logo.svg';
+import Button from '../common/Button';
+import GoogleIcon from '@/assets/icon/google.svg';
+import HelloMoo from '@/assets/moono/moono_hello.svg';
+import { useState } from 'react';
+import { GoogleAuthProvider, signInWithPopup, User } from 'firebase/auth';
+import { auth } from '@/lib/firebase/firebaseClient';
+
 const LoginPage = () => {
+  const [userData, setUserData] = useState<User | null>(null);
+  function hadleGoogleLogin() {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((data) => {
+        setUserData(data.user);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   return (
-    <div>
-      <h1>로그인 페이지</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="mt-[195px]">
+        <HelloMoo />
+      </div>
+      <div className="mt-[30px]">
+        <Logo />
+      </div>
+      <div className="mt-[146px] mb-[117px]">
+        <Button
+          size="full"
+          variant="outline"
+          icon={<GoogleIcon />}
+          onClick={hadleGoogleLogin}
+          className="bg-white"
+        >
+          구글로 로그인
+        </Button>
+      </div>
     </div>
   );
 };
