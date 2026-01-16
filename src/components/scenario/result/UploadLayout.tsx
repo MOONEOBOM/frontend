f39d4e4b2a2b00bedae23a433df524b94ce1d009
moon <gkdlrne97@naver.com>
2026-01-16 19:12:48 +0900

    feat: 업로드 컴포넌트 제작, 스피닝 컴포넌트 제작

---
+++ src/components/scenario/result/UploadLayout.tsx
@@ -0,0 +1,29 @@
+import Button from '@/components/common/Button';
+import { cn } from '@/utils/cn';
+
+const UploadLayout = () => {
+  return (
+    <div
+      className={cn(
+        'flex w-full flex-col items-center gap-[50px] px-[18px] pt-[20px]',
+      )}
+    >
+      <span className="title1 text-center">
+        통화가 종료되었다면 <br />
+        녹음 파일을 업로드해 주세요.
+      </span>
+      <div
+        className={cn(
+          'flex h-[150px] w-full flex-col items-center gap-[25px] rounded-[10px] border border-dashed bg-[var(--color-gray-200)] py-[30px]',
+        )}
+      >
+        <Button className="bg-white" variant="outline" size="half">
+          파일 업로드
+        </Button>
+        <span className="body3">최대 00MB, mp3, wav 파일 지원</span>
+      </div>
+    </div>
+  );
+};
+
+export default UploadLayout;
--- src/components/scenario/result/page.tsx
+++ src/components/scenario/result/page.tsx
@@ -1,9 +1,18 @@
+'use client';
 import Badge from '@/components/common/Badge';
-import Button from '@/components/common/Button';
 import Header from '@/components/common/Header';
 import { cn } from '@/utils/cn';
+import MoonoTako from '@/assets/moono/moono_tako.svg';
+import UploadLayout from './UploadLayout';
+import UploadLoading from './UploadLoading';
+import Modal from '@/components/common/Modal';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 const ScenarioResultPage = () => {
+  const [ExitModal, setExitModal] = useState(false);
+  const [noRecordModal, setNoRecordModal] = useState(false);
+  const router = useRouter();
   return (
     <>
       <Header />
@@ -24,38 +33,50 @@ const ScenarioResultPage = () => {
           <Badge type="blue">요금제 추천</Badge>
         </div>
         <div className={cn('flex flex-col items-center gap-[24px]')}>
-          <span className="script-body-14">
-            통화 내용을 녹음하면 무너가 요약해드려요
+          <span
+            className={cn(
+              'script-body-14 bg-[linear-gradient(to_top,var(--color-primary)_50%,transparent_50%)] px-0.5 leading-relaxed',
+            )}
+          >
+            통화 내용을 녹음하면 무너가 요약해드려요!
           </span>
           <span className="heading4">
             <span className={cn('text-[var(--color-uplus)]')}>LG U+</span>{' '}
             고객센터: 1544-0010
           </span>
         </div>
-        <div className={cn('flex h-100 w-full flex-col border-t border-b')}>
-          {/* 시나리오 내용 */}
-        </div>
         <div
           className={cn(
-            'r flex w-full flex-col items-center gap-[50px] px-[18px] pt-[20px]',
+            'flex h-100 w-full flex-col border-t border-b border-[var(--color-gray-300)]',
           )}
         >
-          <span className="title1 text-center">
-            통화가 종료되었다면 <br />
-            녹음 파일을 업로드해 주세요.
-          </span>
-          <div
-            className={cn(
-              'flex w-full flex-col items-center gap-[25px] rounded-[10px] border border-dashed bg-gray-200 py-[30px]',
-            )}
-          >
-            <Button className="bg-white" variant="outline" size="half">
-              파일 업로드
-            </Button>
-            <span className="body3">최대 00MB, mp3, wav 파일 지원</span>
-          </div>
-          <span className="script-body-16">녹음 파일이 없어요ㅠㅠ</span>
+          {/* 시나리오 내용 */}
         </div>
+        <UploadLayout />
+        {/* <UploadLoading /> */}
+        <span
+          onClick={() => setNoRecordModal(true)}
+          className={cn(
+            'script-body-16 bg-[linear-gradient(to_top,var(--color-primary)_40%,transparent_40%)] px-0.5 leading-relaxed',
+          )}
+        >
+          녹음 파일이 없어요ㅠㅠ
+        </span>
+        <Modal
+          isOpen={noRecordModal}
+          image={<MoonoTako />}
+          type="info"
+          onBack={() => {
+            router.replace('/');
+          }}
+        >
+          무너봄은 음성 파일에 한해
+          <br />
+          상담 요약을 제공해드리고 있어요.
+          <br />
+          <br />
+          불편을 드려 죄송합니다.
+        </Modal>
       </div>
     </>
   );
