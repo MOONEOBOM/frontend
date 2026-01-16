9a90a51df6f5d36b29f615ef38471994cbdf8af2
Park yejin <92977621+yezzan9@users.noreply.github.com>
2026-01-16 12:49:27 +0900

    feat: link button 리팩토링 - onClick을 전달하지 않도록 수정 - props 타입 분리

--- src/components/summary/LinkButton.tsx
+++ src/components/summary/LinkButton.tsx
@@ -1,17 +1,18 @@
+'use client';
+
 import RightChevron from '@/assets/icon/chevron_right.svg?react';
 import React from 'react';
 import { cn } from '@/utils/cn';
-const LinkButton = ({
-  children,
-  type,
-  isFull,
-  onClick,
-}: {
+import Link from 'next/link';
+
+interface LinkButtonProps {
   children: React.ReactNode;
   type: 'outline' | 'solid';
   isFull?: boolean;
-  onClick: () => void;
-}) => {
+  href: string;
+}
+
+const LinkButton = ({ children, type, isFull, href }: LinkButtonProps) => {
   const baseStyle =
     'flex items-center h-[30px] gap-[20px] justify-between body2 rounded-[15px] px-[12px] py-[5px]';
 
@@ -27,10 +28,10 @@ const LinkButton = ({
   );
 
   return (
-    <button type="button" className={className} onClick={onClick}>
+    <Link href={href} type="button" className={className}>
       {children}
       <RightChevron />
-    </button>
+    </Link>
   );
 };
 
