8f587945cdff8a36cab0fc728dc76aa0a5445cc9
Park yejin <92977621+yezzan9@users.noreply.github.com>
2026-01-20 13:46:39 +0900

    feat: 시나리오 종료 경고 모달 수정

--- src/components/common/BackButton.tsx
+++ src/components/common/BackButton.tsx
@@ -3,16 +3,42 @@
 import Link from 'next/link';
 import LeftChevron from '@/assets/icon/chevron_left.svg?react';
 import { useRouter } from 'next/navigation';
+import Modal from './Modal';
+import { useState } from 'react';
 
-const BackButton = ({ type = 'back' }: { type: 'back' | 'home' | 'chat' }) => {
+const BackButton = ({
+  type = 'back',
+}: {
+  type: 'back' | 'home' | 'chat' | 'scenario';
+}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const router = useRouter();
 
+  if (type === 'scenario') {
+    return (
+      <Modal
+        isOpen={isModalOpen}
+        onOverlayClick={() => setIsModalOpen(false)}
+        type="select"
+        onBack={() => setIsModalOpen(false)}
+        onClose={() => router.replace('/')}
+      >
+        시나리오를 종료할까요?
+        <br />
+        지금 종료하면 시나리오는 <br />
+        저장되지 않아요.
+      </Modal>
+    );
+  }
+
   if (type === 'back') {
-    return <LeftChevron onClick={() => router.back()} />;
+    return (
+      <LeftChevron className="cursor-pointer" onClick={() => router.back()} />
+    );
   }
 
   return (
-    <Link href="/">
+    <Link href="/" className="cursor-pointer">
       <LeftChevron />
     </Link>
   );
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,8 +1,12 @@
 import BackButton from '@/components/common/BackButton';
 
-const Header = ({ type = 'back' }: { type: 'back' | 'home' | 'chat' }) => {
+const Header = ({
+  type = 'back',
+}: {
+  type: 'back' | 'home' | 'chat' | 'scenario';
+}) => {
   return (
-    <div className="flex h-[62px] w-full cursor-pointer items-center justify-between px-[20px]">
+    <div className="flex h-[62px] w-full items-center justify-between px-[20px]">
       <BackButton type={type} />
 
       {type == 'chat' ? (
