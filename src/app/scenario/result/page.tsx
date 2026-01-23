867a773a0568f7573a8f9b5b85e4b75ae19270c6
dahlia0916 <dahlia3511@hanyang.ac.kr>
2026-01-23 19:14:46 +0900

    Style

--- src/app/scenario/result/page.tsx
+++ src/app/scenario/result/page.tsx
@@ -1,17 +1,3 @@
 import ScenarioResultPage from '@/components/scenario/result/page';
-import Spinner from '@/components/scenario/result/Spinner';
-import { Suspense } from 'react';
 
-export default function Page() {
-  return (
-    <Suspense
-      fallback={
-        <div className="flex h-screen w-full items-center justify-center">
-          <Spinner size="lg" />
-        </div>
-      }
-    >
-      <ScenarioResultPage />
-    </Suspense>
-  );
-}
+export default ScenarioResultPage;
