8f58ea46e9f45da245d470e78f53243d6dc2ec4c
RyuJaeHwi <yujaehwi02@gmail.com>
2026-01-24 02:30:46 +0900

    fix: 챗봇 상담 대화가 없거나 부족하면 요약 기능 활성화가 막히도록 구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -6,6 +6,7 @@ import LeftChevron from '@/assets/icon/chevron_left.svg?react';
 import ChatModal from '@/components/chatbot/ChatModal';
 import ScenarioModal from '../scenario/result/ScenarioModal';
 import { ChatMessage } from '@/models/summary';
+import { useToastHook } from '@/hooks/useToastHook';
 
 const Header = ({
   type = 'back',
@@ -16,6 +17,13 @@ const Header = ({
 }) => {
   const router = useRouter();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const { toast } = useToastHook();
+
+  /**
+   * 요약 가능한지 판단하는 부분
+   *  : conversation이 존재하고, 1턴 이상의 대화가 있을때만 요약 가능하도록...
+   */
+  const canSummarize = (conversation?.length ?? 0) > 0;
 
   const handleClickLeft = {
     back: () => router.back(),
@@ -35,19 +43,30 @@ const Header = ({
           <button
             type="button"
             className="body2 text-red cursor-pointer"
-            onClick={() => setIsModalOpen(true)}
+            // 요약 가능한 경우에만 모달창 뜨도록 하기 (대화없으면 클릭해도 동작 X)
+            onClick={() => {
+              if (!canSummarize) {
+                toast('negative', '대화가 부족하여 요약 불가능합니다.');
+                return;
+              }
+              setIsModalOpen(true);
+            }}
           >
             상담 종료
           </button>
         )}
       </div>
 
-      {isModalOpen && type === 'chat' && (
+      {/* 대화가 존재하고, 대화가 1턴 이상 지속된 경우에만 모달 작동 */}
+      {isModalOpen &&
+        type === 'chat' &&
+        conversation &&
+        conversation.length > 0 && (
         // 요약 API 관련
         <ChatModal
           isModalOpen={isModalOpen}
           setIsModalOpen={setIsModalOpen}
-          conversation={conversation ?? []}
+          conversation={conversation}
         />
       )}
 
