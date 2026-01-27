150f2f4afee8e542b54b9d09110ab3dfe51df5a5
RyuJaeHwi <yujaehwi02@gmail.com>
2026-01-27 11:49:51 +0900

    fix: vercel 에러 수정

--- src/components/summary/page.tsx
+++ src/components/summary/page.tsx
@@ -2,6 +2,7 @@ import Header from '@/components/common/Header';
 import MoonobomRight from '@/assets/icon/logo_small.svg';
 import SummaryFeedback from './SummaryFeedback';
 import SummaryContent from './SummaryContent';
+import { Suspense } from 'react';
 
 export const dynamic = 'force-dynamic';
 
@@ -14,7 +15,9 @@ const SummaryPage = () => {
       <div className="mb-[60px] flex w-full flex-col items-center gap-[50px] px-[41px]">
         <MoonobomRight width={34} height={32} />
         <div className="h-[1px] w-[250px] bg-gray-300" />
-        <SummaryContent />
+        <Suspense fallback={<div>로딩 중...</div>}>
+          <SummaryContent />
+        </Suspense>
         <div className="h-[1px] w-[250px] bg-gray-300" />
         <SummaryFeedback />
       </div>
