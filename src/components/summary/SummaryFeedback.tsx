513349108d709eff66421e4ec7589fd1789ccf0c
dahlia0916 <dahlia3511@hanyang.ac.kr>
2026-01-28 11:05:31 +0900

    Fix: 컨페티 화면 사이즈 조정

--- src/components/summary/SummaryFeedback.tsx
+++ src/components/summary/SummaryFeedback.tsx
@@ -7,45 +7,53 @@ import Confetti from 'react-confetti';
 import { useState } from 'react';
 const SummaryFeedback = () => {
   const router = useRouter();
-  const { width, height } = window.screen;
   const [showConfetti, setShowConfetti] = useState(false);
   const handleRouter = () => {
     setShowConfetti(true);
     setTimeout(() => {
       router.push('/');
-    }, 2000);
+    }, 2500);
   };
 
   return (
-    <div className="flex flex-col items-center gap-[30px]">
+    <>
       {showConfetti && (
         <Confetti
-          width={width}
-          height={height}
+          width={390}
+          height={844}
           numberOfPieces={500}
           gravity={0.5}
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: '50%',
+            transform: 'translateX(-50%)',
+            zIndex: 100,
+          }}
         />
       )}
-      <p className="heading2">무너의 요약은 어떠셨나요?</p>
-      <div className="flex gap-[25px]">
-        <motion.button
-          initial={{}}
-          whileHover={{ scale: 1.2 }}
-          className="cursor-pointer"
-          onClick={handleRouter}
-        >
-          <MoonoGood />
-        </motion.button>
-        <motion.button
-          initial={{}}
-          whileHover={{ scale: 1.2 }}
-          className="cursor-pointer"
-          onClick={handleRouter}
-        >
-          <MoonoBad />
-        </motion.button>
+      <div className="flex flex-col items-center gap-[30px]">
+        <p className="heading2">무너의 요약은 어떠셨나요?</p>
+        <div className="flex gap-[25px]">
+          <motion.button
+            initial={{}}
+            whileHover={{ scale: 1.2 }}
+            className="cursor-pointer"
+            onClick={handleRouter}
+          >
+            <MoonoGood />
+          </motion.button>
+          <motion.button
+            initial={{}}
+            whileHover={{ scale: 1.2 }}
+            className="cursor-pointer"
+            onClick={handleRouter}
+          >
+            <MoonoBad />
+          </motion.button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
