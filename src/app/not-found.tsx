5c552e0baef7e29701647eedbf7483c1d40d7f67
RyuJaeHwi <yujaehwi02@gmail.com>
2026-01-28 16:39:50 +0900

    fix: 404 에러페이지 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,9 +2,7 @@ import ErrorPage from "@/components/common/ErrorPage";
 
 const NotFound = () => {
   return (
-    <ErrorPage
-      message={`앗! 찾으시는 페이지가 없어요.\n올바른 주소인지 다시 확인해 주시겠어요?`}
-    />
+    <ErrorPage />
   );
 };
 
--- src/components/common/ErrorPage.tsx
+++ src/components/common/ErrorPage.tsx
@@ -5,13 +5,13 @@ import MoonoError from '@/assets/moono/moono_lost.svg?react';
 import Button from '@/components/common/Button';
 
 interface ErrorPageProps {
-  message: string;
+  message?: string;
   buttonColor?: string;
   reset?: () => void;
 }
 
 export default function ErrorPage({
-  message,
+  message = "앗! 무너가 길을 잃었어요.\n잠시 후 다시 시도해 주시겠어요?",
   buttonColor,
   reset,
 }: ErrorPageProps) {
