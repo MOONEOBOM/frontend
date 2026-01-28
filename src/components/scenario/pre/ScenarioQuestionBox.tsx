6aa70cba6c950b6b72a03ffdd7f82420997f3e82
moon <gkdlrne97@naver.com>
2026-01-28 17:23:27 +0900

    fix: 시나리오사전질문페이지 오타수정

--- src/components/scenario/pre/ScenarioQuestionBox.tsx
+++ src/components/scenario/pre/ScenarioQuestionBox.tsx
@@ -81,7 +81,7 @@ const ScenarioQuestionBox = ({
           <>
             {/* 3. 이유 질문 */}
             <div className="mt-[53px]">
-              <p className="heading3">변경하시는 이유가 무엇인가요?</p>
+              <p className="heading3">상담할 내용을 선택해 주세요.</p>
             </div>
 
             {/* 4. 이유 선택 Dropdown */}
